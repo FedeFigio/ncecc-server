36842219ccbe028f3f4160e904aa229aa92e8227
Federico <federicofigini90@gmail.com>
2022-01-24 23:59:27 +0100

    wip

--- server.js
+++ server.js
@@ -1,29 +1,63 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const uniqid = require('uniqid');
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
+    cors: {
+        origin: "*",
+    },
 });
 
 const rooms = [];
+const users = [];
+const socketUser = {};
 
 io.on("connection", (socket) => {
-  socket.on("start-hosting", (data) => {
-    const newRoom = {
-      id: `${data.room}:12345`,
-      users: [data.username],
-    };
-    rooms.push(newRoom);
-    socket.join(newRoom.id);
-    socket.emit("hosting-started", newRoom);
-  });
+    console.log('connected', socket.id);
+
+    socket.on("disconnect", (reason) => {
+        const user = users.find(r => r.id == socketUser[socket.id]);
+
+        console.log('disconnected room', user, socket.id);
+    });
+
+    socket.on("create-room", (data) => {
+        const newRoom = {
+            id: getRoomIdentifier(data.room),
+        };
+        rooms.push(newRoom);
+        socket.join(newRoom.id);
+
+        const user = {
+            id: uniqid(),
+            socketId: socket.id,
+            roomId: newRoom.id,
+            username: data.username,
+        }
+        users.push(user);
+        socketUser[socket.id] = user.id;
+
+        socket.emit("room-created", newRoom, user);
+    });
+
+    socket.on("request-start", (user) => {
+
+    });
 });
 
 httpServer.listen(3001, () => {
-  console.log("Linstening on port 3001...");
+    console.log("Linstening on port 3001...");
 });
+
+function getRoomIdentifier(roomName) {
+    const random = Math.floor(Math.random() * 10000);
+    const stringId = `00000${String(random)}`.slice(-5);
+    const roomId = `${roomName}#${stringId}`;
+
+    const roomExists = rooms.findIndex(r => r.id == roomId) > -1;
+    if (roomExists) return getRoomIdentifier(roomName);
+    return roomId;
+}
